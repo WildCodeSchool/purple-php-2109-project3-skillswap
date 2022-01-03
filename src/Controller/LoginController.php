d1b38cfc8387aeebb0b47491263167259dffa2f2
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-03 18:30:55 +0100

    finaly create user and login and logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -24,12 +24,12 @@ class LoginController extends AbstractController
         ]);
     }
 
-     /**
-     * @Route("/logout", name="logout", methods={"GET"})
-     */
+    /**
+    * @Route("/logout", name="logout", methods={"GET"})
+    */
     public function logout(): void
     {
         // controller can be blank: it will never be called!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+      /*   throw new \Exception('Don\'t forget to activate logout in security.yaml'); */
     }
 }
