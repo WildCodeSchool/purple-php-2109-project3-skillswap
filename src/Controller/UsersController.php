f756bf734cd2eda4c5221a7a300a9a064638b6c0
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-06 15:55:05 +0100

    correction faill security

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/users")
