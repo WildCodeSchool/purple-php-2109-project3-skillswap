05cde9a02778f0c49afb27cde4fa7791a2ace86c
Olivier Gourdonneau <olivier.gourdonneau@meteo.fr>
2022-01-11 11:32:31 +0100

    add controller and form for comments

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/comment", name="comment")
+ */
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/", name="")
+     */
+    public function index(Request $request): Response
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /* if (is_string($form->get('sender')->getData()) && (is_string($this->get('recipient')))) { */
+                return $this->redirectToRoute("comment_valid");
+            /*} */
+        }
+        return $this->render("comment/index.html.twig", [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/valid", name="_valid")
+     */
+    public function valid(Request $request): Response
+    {
+        return $this->render("comment/valid.html.twig");
+    }
+}
