19bbeda9e99923b285fb7082d6568c19230a977f
sebastien-violante <sebastien.violante@gmail.com>
2022-01-12 16:11:27 +0100

    code quality correction on UserSkillType.php

--- src/Form/UserSkillType.php
+++ src/Form/UserSkillType.php
@@ -7,9 +7,7 @@ use App\Entity\Skill;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 
 class UserSkillType extends AbstractType
 {
