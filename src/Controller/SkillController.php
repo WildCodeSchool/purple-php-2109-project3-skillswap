2fd6bf5fe35e4f6d357d85678cc13c385376cdc6
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-13 16:35:05 +0100

    modify css on several pages

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -18,7 +18,7 @@ class SkillController extends AbstractController
     /**
      * Sends to the view the list of skills currently in the database in alphabetical
      * order and the categories in the class skill.
-     * @Route("/research", name="skills_research", methods={"GET"})
+     * @Route("/research", name="research", methods={"GET"})
      */
     public function research(SkillRepository $skillRepository): Response
     {
