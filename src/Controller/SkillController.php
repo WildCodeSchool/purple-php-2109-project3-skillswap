518624ad7cef2758ce410ce21e631ba498d5dba4
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-13 09:29:51 +0100

    correctif conflict

--- src/Controller/SkillController.php
+++ src/Controller/SkillController.php
@@ -11,12 +11,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/skills")
+ * @Route("/skills", name="skills_")
  */
 class SkillController extends AbstractController
 {
     /**
-     * @Route("/research", name="skills_research", methods={"GET"})
+     * @Route("/research", name="research", methods={"GET"})
      */
     public function research(SkillRepository $skillRepository): Response
     {
@@ -24,16 +24,6 @@ class SkillController extends AbstractController
         return $this->render('skills/research.html.twig', [
             'skills' => $skillRepository->findBy([], ['category' => 'ASC']),
             'categories' => Skill::CATEGORIES,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="skills_show", methods={"GET"})
-     */
-    public function show(Skill $skill): Response
-    {
-        return $this->render('skills/show.html.twig', [
-            'skill' => $skill,
         ]);
     }
 }
