c527d9ed8fa1f4da740db875bd971ca2a78ba2a9
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-16 00:33:16 +0100

    show and add skil

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Skill;
+use App\Form\SkillType;
+use App\Repository\SkillRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin", name="admin_")
@@ -20,5 +25,31 @@ class AdminController extends AbstractController
     public function dashboard(): Response
     {
         return $this->render('admin/dashboard.html.twig');
+    }
+
+    /**
+     * show, add and modify the list of skills
+     * @Route("/skill", name="skill")
+     */
+    public function showSkill(
+        Request $request,
+        SkillRepository $skillRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+
+        $skill = new Skill();
+        $form = $this->createForm(SkillType::class, $skill);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($skill);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_skill', [], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('admin/skill.html.twig', [
+            'skills' => $skillRepository->findBy([], ['name' => 'ASC']),
+            'form' => $form,
+        ]);
     }
 }
