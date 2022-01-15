329d01e818f7492423a25af2f0770ad96912194d
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-15 19:49:14 +0100

    see the users for the administrator

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin", name="admin_")
@@ -20,5 +22,27 @@ class AdminController extends AbstractController
     public function dashboard(): Response
     {
         return $this->render('admin/dashboard.html.twig');
+    }
+
+    /**
+     * displays the list of users
+     * @Route("/users", name="users")
+     */
+    public function showSeason(UserRepository $userRepository): Response
+    {
+        return $this->render('admin/users.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * displays the details of a user
+     * @Route("/users{id}", name="user_show", methods={"GET"})
+     */
+    public function show(User $user): Response
+    {
+        return $this->render('admin/user_show.html.twig', [
+            'user' => $user,
+        ]);
     }
 }
