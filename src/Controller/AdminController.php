401fd37a56ecb799efa78300b15dedd19e29988f
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-15 21:56:23 +0100

    see the users for the administrator

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -28,7 +28,7 @@ class AdminController extends AbstractController
      * displays the list of users
      * @Route("/users", name="users")
      */
-    public function showSeason(UserRepository $userRepository): Response
+    public function showUsers(UserRepository $userRepository): Response
     {
         return $this->render('admin/users.html.twig', [
             'users' => $userRepository->findAll(),
@@ -39,7 +39,7 @@ class AdminController extends AbstractController
      * displays the details of a user
      * @Route("/users{id}", name="user_show", methods={"GET"})
      */
-    public function show(User $user): Response
+    public function showOneUser(User $user): Response
     {
         return $this->render('admin/user_show.html.twig', [
             'user' => $user,
