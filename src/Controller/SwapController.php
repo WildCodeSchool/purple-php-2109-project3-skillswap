0566421cfdfe9334ff28dd9325c5ea8d6474f049
Benjamin-51 <babilliot.benjamin@gmail.com>
2022-01-18 17:23:47 +0100

    Fix for the two methods that were overwritten.
    I am not sure what happened since no commit shows that the file was deleted after creating this branch from dev.

--- src/Controller/SwapController.php
+++ src/Controller/SwapController.php
@@ -5,20 +5,46 @@ namespace App\Controller;
 use DateTime;
 use App\Entity\Swap;
 use App\Entity\User;
+use App\Entity\Skill;
 use App\Form\SwapType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
-* @Route("/swap", name="swap_")
+* @Route("/swap")
 */
 class SwapController extends AbstractController
 {
     /**
-    * @Route("/{id} ", name="form", methods={"GET", "POST"}, requirements={"id"="\d+"})
+     * The method returns the swappers who declared themselves competent on the skill in parameter
+     * @Route("/research/{id}", name="swap_research", requirements={"id"="\d+"})
+     */
+    public function research(Skill $skill): Response
+    {
+        return $this->render('swap/research.html.twig', [
+            'skill' => $skill
+        ]);
+    }
+
+    /**
+     * @Route("/display/{skill_id}/{user_id}", name="swapper_display")
+     * @ParamConverter("skill",class="App\Entity\Skill", options = {"mapping": {"skill_id": "id"}})
+     * @ParamConverter("user",class="App\Entity\User", options = {"mapping": {"user_id": "id"}})
+     */
+    public function display(Skill $skill, User $user): Response
+    {
+        return $this->render('swapper/display.html.twig', [
+            'user' => $user,
+            'skill' => $skill,
+        ]);
+    }
+
+    /**
+    * @Route("/{id} ", name="swap_form", methods={"GET", "POST"}, requirements={"id"="\d+"})
     */
     public function index(
         User $helper,
