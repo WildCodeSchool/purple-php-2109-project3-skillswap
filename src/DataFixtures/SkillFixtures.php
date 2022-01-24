c68816d209370b1d8988f76163340f3694618cda
Olivier Gourdonneau <olivier.gourdonneau@meteo.fr>
2022-01-24 12:24:40 +0100

    DRY code for Skill accordion

--- src/DataFixtures/SkillFixtures.php
+++ src/DataFixtures/SkillFixtures.php
@@ -14,109 +14,109 @@ class SkillFixtures extends Fixture
     public const SKILLS = [
         [
             'name' => 'Achat',
-            'category' => Skill::CATEGORIES[2],
+            'category' => Skill::CATEGORIES['Three'],
         ],[
             'name' => 'Import-Export',
-            'category' => Skill::CATEGORIES[2],
+            'category' => Skill::CATEGORIES['Three'],
         ],[
             'name' => 'Stratégie commerciale',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Vente',
-            'category' => Skill::CATEGORIES[2],
+            'category' => Skill::CATEGORIES['Three'],
         ],[
             'name' => 'Community Manager',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Conception Rédaction',
-            'category' => Skill::CATEGORIES[4],
+            'category' => Skill::CATEGORIES['Five'],
         ],[
             'name' => 'Evénementiel',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Graphisme',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Relations publiques',
-            'category' => Skill::CATEGORIES[4],
+            'category' => Skill::CATEGORIES['Five'],
         ],[
             'name' => 'Comptabilité',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Architecte d \'intérieur',
-            'category' => Skill::CATEGORIES[4],
+            'category' => Skill::CATEGORIES['Five'],
         ],[
             'name' => 'Designer produit',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Contrôle des finances',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Crowdfunding',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Contrôle de gestion ',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'R&D',
-            'category' => Skill::CATEGORIES[4],
+            'category' => Skill::CATEGORIES['Five'],
         ],[
             'name' => 'Développement web',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Gestion de projet informatique',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'Conseil jurudique',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Fiscalité',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Service d\'avocat',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Droit des sociétés',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Contrat de travail',
-            'category' => Skill::CATEGORIES[0],
+            'category' => Skill::CATEGORIES['One'],
         ],[
             'name' => 'Analyse de data',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Pricing',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Marketing digital',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Marketing produit',
-            'category' => Skill::CATEGORIES[1],
+            'category' => Skill::CATEGORIES['Two'],
         ],[
             'name' => 'Paie',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'Formation',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'Recrutement',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'Stratégie RH',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'Secrétariat',
-            'category' => Skill::CATEGORIES[3],
+            'category' => Skill::CATEGORIES['Four'],
         ],[
             'name' => 'Photographie',
-            'category' => Skill::CATEGORIES[4],
+            'category' => Skill::CATEGORIES['Five'],
         ],[
             'name' => 'Affrêteur',
-            'category' => Skill::CATEGORIES[2],
+            'category' => Skill::CATEGORIES['Three'],
         ],[
             'name' => 'Conditionnement',
-            'category' => Skill::CATEGORIES[2],
+            'category' => Skill::CATEGORIES['Three'],
         ],
     ];
 
