641fc4ae452b4e0348c5a6be97128a942ca82eaf
Benjamin Babilliot <82387760+Benjamin-51@users.noreply.github.com>
2022-01-26 10:47:13 +0100

    Dead code cleanup

--- src/Repository/SwapRepository.php
+++ src/Repository/SwapRepository.php
@@ -18,26 +18,4 @@ class SwapRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Swap::class);
     }
-
-    public function getSwapDashboard(int $skill, int $partner, int $user): array
-    {
-        $queryBuilder = $this->createQueryBuilder('s')
-            ->select('s')
-            ->where('s.skill = :skill AND (
-                (s.asker = :partner AND s.helper = :user)
-                OR
-                (s.asker = :user AND s.helper = :partner)
-                )')
-            ->setParameter('skill', $skill)
-            ->setParameter('partner', $partner)
-            ->setParameter('user', $user)
-            ->orderBy('s.date', 'DESC')
-            ->getQuery();
-
-        if (!is_array($queryBuilder->getResult())) {
-            return([]);
-        }
-
-        return $queryBuilder->getResult();
-    }
 }
