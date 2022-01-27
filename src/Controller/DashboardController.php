5ce93c94b73adf9a98f30968d8207343d9e4b0c8
Olivier Gourdonneau <olivier.gourdonneau@meteo.fr>
2022-01-27 17:02:04 +0100

    created dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,23 +2,31 @@
 
 namespace App\Controller;
 
-use App\Repository\SwapRepository;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DashboardController extends AbstractController
 {
     /**
      * @Route("/dashboard", name="dashboard")
+     * @IsGranted("ROLE_USER")
      */
-    public function index(
-        SwapRepository $swapRepository
-    ): Response {
-        $swaps = $swapRepository->findBy(["helper" => $this->getUser()]);
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        if ($user instanceof User) {
+            $swapsAsked = $user->getAskedSwaps();
+            $swapsHelped = $user->getHelpedSwaps();
 
-        return $this->render('dashboard/index.html.twig', [
-            'swaps' => $swaps,
-        ]);
+            return $this->render('dashboard/index.html.twig', [
+                'swaps_asked' => $swapsAsked,
+                'swaps_helped' => $swapsHelped,
+            ]);
+        } else {
+            return $this->redirectToRoute('home');
+        }
     }
 }
